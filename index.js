aa1a36e3f46c94df385ebcbd33a11daaccbe0f5b
parsercoder <parsercoder@gmail.com>
2020-04-15 00:05:06 +0200

    adding test function for sayHi

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 function sayHi(name) {
   return `hi ${name}`
 }
+
+function test() {
+  const testSayHi = sayHi("elsiddig")
+
+  if (typeof testSayHi === "string") {
+    console.log("passed")
+  } else {
+    console.log("failed")
+  }
+}
